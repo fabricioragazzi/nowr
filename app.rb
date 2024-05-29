3427da9dbaf9f02839487e9505682e24817a158f
leandronsp <leandronsp@gmail.com>
2024-05-29 18:51:59 -0300

    add new word

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ puts "Listening to the port 3000..."
 loop do
   client = server.accept
 
-  response = "HTTP/2 200 OK\r\nContent-Type: text/html\r\nContent-Length:22\r\n\r\n<h1>Hello</h1>"
+  response = "HTTP/2 200 OK\r\nContent-Type: text/html\r\nContent-Length:22\r\n\r\n<h1>Hello, world</h1>"
   client.puts(response)
 
   client.close
