5563b6a1d2f7e7cb0005695142a4915ab54ab0ea
leandronsp <leandronsp@gmail.com>
2024-06-20 18:59:42 -0300

    display time on browser

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require 'socket'
+require 'yaml'
+require 'pg'
 
 server = TCPServer.new('0.0.0.0', 3000)
+
+dbconfig = YAML.load_file('dbconfig.yml')
+database = PG.connect(**dbconfig)
 
 puts "Listening to the port 3000..."
 
 loop do
   client = server.accept
 
-  response = "HTTP/2 200 OK\r\nContent-Type: text/html\r\nContent-Length:22\r\n\r\n<h1>It works!</h1>"
+  time = database.exec("SELECT NOW()").first["now"]
+  response = "HTTP/2 200 OK\r\nContent-Type: text/html\r\nContent-Length:30\r\n\r\n<h1>#{time}</h1>"
   client.puts(response)
 
   client.close
--- app_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-
-class AppTest < Test::Unit::TestCase
-    def test_app
-        assert_equal(1, 1)
-    end
-end
-
